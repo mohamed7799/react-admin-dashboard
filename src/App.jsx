4d0e6cfff80035f950e1065add67dc23ebf67214
mohmed <mohmedamged9@gmail.com>
2022-12-13 16:52:40 +0200

    build the side bar component (UI)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import SideBar from "./components/sideBar";
 import TopBar from "./components/TopBar";
 
 const App = () => {
   return (
     <main className="font-body ">
       <TopBar></TopBar>
+      <div className="flex ">
+        <div className="flex-1 ">
+          <SideBar></SideBar>
+        </div>
+
+        <div className="flex-[4_4_0%] ">other content</div>
+      </div>
     </main>
   );
 };
---
+++ src/components/sideBar.jsx
@@ -0,0 +1,89 @@
+import {
+  LineStyle,
+  Timeline,
+  PersonOutline,
+  Storefront,
+  AttachMoney,
+  Leaderboard,
+  MailOutline,
+  Feedback,
+  ChatBubbleOutline,
+  Error,
+} from "@mui/icons-material";
+
+const SideBar = () => {
+  return (
+    <div className="px-4">
+      <div>
+        <h3 className="sideBar-menu-title ">Dashboard</h3>
+        <ul className=" text-gray-700">
+          <li className="sideBar-menu-list-item">
+            <LineStyle /> <span>Home</span>
+          </li>
+          <li className="sideBar-menu-list-item">
+            <Timeline />
+            <span> Analytics</span>
+          </li>
+          <li className="sideBar-menu-list-item">
+            <Timeline />
+            <span> Sales</span>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3 className="sideBar-menu-title ">Quick menu</h3>
+        <ul className=" text-gray-700">
+          <li className="sideBar-menu-list-item">
+            <PersonOutline /> <span>Users</span>
+          </li>
+          <li className="sideBar-menu-list-item">
+            <Storefront />
+            <span> Products</span>
+          </li>
+          <li className="sideBar-menu-list-item">
+            <AttachMoney />
+            <span> Transactions</span>
+          </li>
+          <li className="sideBar-menu-list-item">
+            <Leaderboard />
+            <span> Reports</span>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3 className="sideBar-menu-title ">Notifications</h3>
+        <ul className=" text-gray-700">
+          <li className="sideBar-menu-list-item">
+            <MailOutline /> <span>Mail</span>
+          </li>
+          <li className="sideBar-menu-list-item">
+            <Feedback />
+            <span> Feedback</span>
+          </li>
+          <li className="sideBar-menu-list-item">
+            <ChatBubbleOutline />
+            <span> Messages</span>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3 className="sideBar-menu-title ">Staff</h3>
+        <ul className=" text-gray-700">
+          <li className="sideBar-menu-list-item">
+            <MailOutline /> <span>Manage</span>
+          </li>
+          <li className="sideBar-menu-list-item">
+            <Timeline />
+            <span> Analytics</span>
+          </li>
+          <li className="sideBar-menu-list-item">
+            <Error />
+            <span> Reports</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
